deacd77a217d3335ec8c6d04843625c4f25ffe38
Andrew Walden <andrew@weblinxinc.com>
2017-05-04 17:11:36 -0400

    Set up test cases and pools

--- main.pl
+++ main.pl
@@ -11,8 +11,6 @@ require "./include.pl";
 my @vars = ("x", "y", "z");
 my @test_cases;
 
-for my $i (0..9) {
-    my $expr = get_random_expr(10, \@vars);
-    my $binding = get_random_binding(\@vars);
-    run_expr($expr, $binding);
-}
+my $pool = make_pool(10, \@vars);
+my $test_cases = make_test_cases(10, \@vars);
+run_test_cases($pool, $test_cases);
--- scoring/pools.pl
+++ scoring/pools.pl
@@ -19,27 +19,59 @@ sub make_pool {
 }
 
 #
-# Prints the expression pool and their evaluation result
+# Tests the pool against the binding and expected result
 #
-sub print_pool {
-
-}
-
 sub test_pool {
+	my $pool_ref = $_[0];
+	my @pool = @$pool_ref;
+	my $pool_size = @pool;
+	my $test_case_ref = $_[1];
+	my %test_case = %$test_case_ref;
+	my $binding_ref = $test_case{binding};
+	my $expected_result = $test_case{expected};
 
+	foreach my $i (0..($pool_size - 1)) {
+		my $result = eval_expr($pool[$i], $binding_ref);
+		print "$i> ";
+		print_expr($pool[$i]);
+		print "    expected : $expected_result\n";
+		print "    actual   : $result\n";
+		if ($result == $expected_result) {
+			print "    !! PASSED !!\n";
+		}
+		else {
+			print "    !! FAILED !!\n";
+		}
+	}
 }
 
-sub print_test_results {
-
-
+sub make_test_case {
+	my $vars_ref = $_[0];
+	my %test_case;
+	$test_case{binding} = get_random_binding($vars_ref);
+	$test_case{expected} = int(rand() * 10);
+	return \%test_case;
 }
 
-sub add_test_case {
+sub make_test_cases {
+	my $num_test_cases = $_[0];
+	my $vars_ref = $_[1];
+	my @test_cases;
 
+	foreach my $i (0..($num_test_cases - 1)) {
+		$test_cases[$i] = make_test_case($vars_ref);
+	}
+
+	return \@test_cases;
 }
 
-sub mk_test_case {
-
+sub run_test_cases {
+	my $pool_ref = $_[0];
+	my $test_cases_ref = $_[1];
+	my @test_cases = @$test_cases_ref;
+	my $num_test_cases = @test_cases;
+	foreach my $i (0..($num_test_cases - 1)) {
+		test_pool($pool_ref, $test_cases[$i]);
+	}
 }
-
 1;
